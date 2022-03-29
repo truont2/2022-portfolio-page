0e932cca5a9b7b31171cfb49a9d7656f34963ed7
Takara Truong <truont2@gmail.com>
2022-03-28 20:36:38 -0700

    working copy

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,10 +1,4 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
